bfc97dfca52261b7d1abb3e4c72deaae8253bc14
Rafal Kural <silvidanfox@gmail.com>
2015-06-25 20:29:06 +0100

    Added argument to select or unselect annotations on plots

--- Lecture script/generate_plots_pdf.r
+++ Lecture script/generate_plots_pdf.r
@@ -1,12 +1,16 @@
-generate_plots_pdf <- function(SCORES,samplenames,CLASS,varvector,filename){
+generate_plots_pdf <- function(SCORES,samplenames,CLASS,varvector,filename,annotate){
   #generates 3 plots: PC1vPC2, PC2vPC3, PC1vPC3
   #outputs 1 pdf file
   #SCORES - pca scores matrix
   #samplenames - matrix with sample names
   #varvector - vector of eigenvalues for eigen pca, W vector for SVD
   #filename - name for the pdf
+  #annotate - TRUE to annotate samples on plot, default FALSE
   #
   #Rafal Kural
+  if (missing(annotate)) {
+    annotate = FALSE
+  }
   variance <- c(varvector[1] / sum(varvector) * 100,
                 varvector[2] / sum(varvector) * 100,
                 varvector[3] / sum(varvector) * 100)
@@ -18,6 +22,7 @@ generate_plots_pdf <- function(SCORES,samplenames,CLASS,varvector,filename){
   #vector of colors of size of X storing colors appropriate for given sample class
   colorsVector <- NULL
   for (i in 1:length(colors)) {
+    #last element is NA, ignore it
     if (is.na(classAppearances[i]))
     {
       break
@@ -28,17 +33,23 @@ generate_plots_pdf <- function(SCORES,samplenames,CLASS,varvector,filename){
   pdf(filename)
   par(mar=c(5.1, 4.1, 4.1, 8.1), xpd=TRUE)
   plot(SCORES[,1], SCORES[,2], col=colorsVector, xlab = sprintf("PC1 (%1.0f%%)", variance[1]), ylab = sprintf("PC2 (%1.0f%%)", variance[2]), main = "PC1 vs PC2")
-  text(SCORES[,1], SCORES[,2], as.character(samplenames), col="gray")
+  if (annotate == TRUE) {
+    text(SCORES[,1], SCORES[,2], as.character(samplenames), col="gray")
+  }
   legend("topright", inset=c(-0.2,0), legend=unique(CLASS), cex=0.8, col=colors, pch=1)
 
   par(mar=c(5.1, 4.1, 4.1, 8.1), xpd=TRUE)
   plot(SCORES[,2], SCORES[,3], col=colorsVector, xlab = sprintf("PC2 (%1.0f%%)", variance[2]), ylab = sprintf("PC3 (%1.0f%%)", variance[3]), main = "PC2 vs PC3")
-  text(SCORES[,2], SCORES[,3], as.character(samplenames), col="gray")
+  if (annotate == TRUE) {
+    text(SCORES[,1], SCORES[,2], as.character(samplenames), col="gray")
+  }
   legend("topright", inset=c(-0.2,0), legend=unique(CLASS), cex=0.8, col=colors, pch=1)
 
   par(mar=c(5.1, 4.1, 4.1, 8.1), xpd=TRUE)
   plot(SCORES[,1], SCORES[,3], col=colorsVector, xlab = sprintf("PC1 (%1.0f%%)", variance[1]), ylab = sprintf("PC3 (%1.0f%%)", variance[3]), main = "PC1 vs PC3")
-  text(SCORES[,1], SCORES[,3], as.character(samplenames), col="gray")
+  if (annotate == TRUE) {
+    text(SCORES[,1], SCORES[,2], as.character(samplenames), col="gray")
+  }
   legend("topright", inset=c(-0.2,0), legend=unique(CLASS), cex=0.8, col=colors, pch=1)
   dev.off()
 }
