b87b54d19238b2c71317cdb5b4ee824956bc86c3
Rafal Kural <silvidanfox@gmail.com>
2015-06-29 17:14:31 +0100

    Modified script so test set can be easily changed.

--- Lecture script/2.SVM_model.r
+++ Lecture script/2.SVM_model.r
@@ -7,14 +7,19 @@
 require(e1071)
 
 ## !! consider ensuring that test array has representatives of ALL classes) !! ##
+
+###### SETTING TRAINING AND TEST DATA ####################
 #retrieve training variables
 Xtrain <- X[-seq(1, nrow(X), 4),] #original data
 #SCORES_SVD <- SCORESrs[,1:9]
 #Xtrain <- SCORES_SVD[-seq(1, nrow(SCORES_SVD), 4),] #PCA scores
 CLASStrain <- CLASS[-seq(1, length(CLASS), 4)]
-#retrieve test variables (testing with entire data so commented it)
-#Xtest <- X[seq(1, nrow(X), 10),]
-#CLASStest <- CLASS[seq(1, length(CLASS), 10)]
+#setting test arrays (use either full data or part of it)
+Xtest <- X[seq(1, nrow(X), 4),]
+CLASStest <- CLASS[seq(1, length(CLASS), 4)]
+#Xtest <- X
+#CLASStest <- CLASS
+###### END ###############################################
 
 #### SETTINGS START ###################################
 # Settings allow automating analysis process
@@ -22,13 +27,13 @@ CLASStrain <- CLASS[-seq(1, length(CLASS), 4)]
 kernelType = c("radial", "polynomial")
 # Cost - determines cost of violating separation margin
 # Highes Cost values makes it harder to violate margin, default 1, allows vector of values
-cost = c(0.5, 1, 2, 3, 4)
+cost = c(1, 3, 4)
 # gamma - variable influencing radial separation, default 1/numberOfVariablesPerSample, allows vector of values
-gammaVar <- c(0.5, 1, 2, 3, 4, 5)
+gammaVar <- c(1, 2, 3, 4, 5)
 # degree - degree of polynomial used for separation in polynomial kernel, allows vector of values
-degreeVar <- c(3, 4, 5, 6, 7, 8, 9, 10)
+degreeVar <- c(3, 5, 7, 9)
 # additional file append - scaling or other, will be added to result file in form Result_VM_addFileAppend_rest
-addFileAppend <- "noscale_0.75"
+addFileAppend <- "noscale_FTIR_0.75"
 #### SETTINGS END ######################################
 
 #iterate over all chosen kernels
@@ -44,13 +49,13 @@ for (kernelType_temp in kernelType) {
         #train model
         SVM_model <- svm(Xtrain, CLASStrain, scale=FALSE,type="C-classification",kernel=kernelType_temp, gamma=gammaVar_temp/ncol(Xtrain), cost=cost_temp)
         #predict classes
-        CLASSpredicted <- (predict(SVM_model,X)) # original variables
+        CLASSpredicted <- (predict(SVM_model,Xtest))
         #CLASSpredicted <- (predict(SVM_model,SCORES_SVD)) #PCA scores
         
         #create hit/miss vector
         hitmiss <- c(rep("0", length(CLASSpredicted)))
         for (i in 1:length(CLASSpredicted)) {
-          if (CLASS[i] == CLASSpredicted[i]) {
+          if (CLASStest[i] == CLASSpredicted[i]) {
             hitmiss[i] <- "1"
           }
         }
@@ -60,9 +65,9 @@ for (kernelType_temp in kernelType) {
         #display accuracy
         cat(paste(accuracy, "\n",sep = ""))
         #create frame for results of test
-        resultTable <- data.frame(CLASS, CLASSpredicted, hitmiss, accuracy)
+        resultTable <- data.frame(CLASStest, CLASSpredicted, hitmiss, accuracy)
         #save results table in csv file
-        write.csv(resultTable, file = paste("../SVM_results/Result_VM_", addFileAppend, "_", kernelType_temp, "_C", cost_temp, "_gamma", gammaVar_temp, ".csv", sep=""))      
+        write.csv(resultTable, file = paste("../SVM_results/Result_", addFileAppend, "_", kernelType_temp, "_C", cost_temp, "_gamma", gammaVar_temp, ".csv", sep=""))      
         }
     }
   }
@@ -76,13 +81,13 @@ for (kernelType_temp in kernelType) {
         #train model
         SVM_model <- svm(Xtrain, CLASStrain, scale=FALSE,type="C-classification",kernel=kernelType_temp, degree=degreeVar_temp, cost=cost_temp)
         #predict classes
-        CLASSpredicted <- (predict(SVM_model,X)) #original variables
+        CLASSpredicted <- (predict(SVM_model,Xtest))
         #CLASSpredicted <- (predict(SVM_model,SCORES_SVD)) #PCA scores
         
         #create hit/miss vector
         hitmiss <- c(rep("0", length(CLASSpredicted)))
         for (i in 1:length(CLASSpredicted)) {
-          if (CLASS[i] == CLASSpredicted[i]) {
+          if (CLASStest[i] == CLASSpredicted[i]) {
             hitmiss[i] <- "1"
           }
         }
@@ -92,9 +97,9 @@ for (kernelType_temp in kernelType) {
         #display accuracy
         cat(paste("Accuracy: ", accuracy, "\n",sep = ""))
         #create frame for results of test
-        resultTable <- data.frame(CLASS, CLASSpredicted, hitmiss, accuracy)
+        resultTable <- data.frame(CLASStest, CLASSpredicted, hitmiss, accuracy)
         #save results in csv file
-        write.csv(resultTable, file = paste("../SVM_results/Result_VM_", addFileAppend, "_", kernelType_temp, "_C", cost_temp, "_degree", degreeVar_temp, ".csv", sep=""))      
+        write.csv(resultTable, file = paste("../SVM_results/Result_", addFileAppend, "_", kernelType_temp, "_C", cost_temp, "_degree", degreeVar_temp, ".csv", sep=""))      
       }
     }
   }
