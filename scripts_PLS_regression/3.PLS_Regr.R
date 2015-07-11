a5929ddfa395775b47f78c399a3a1d6b2605cc77
Rafal Kural <silvidanfox@gmail.com>
2015-07-11 22:34:44 +0100

    Split script in settings and algorithm parts. Modified script to use test set instead of training (hard coded for plsreg1 from plsdepot). Added plot drawing for all ncomps in single pdf.

--- scripts_PLS_regression/3.PLS_Regr.R
+++ scripts_PLS_regression/3.PLS_Regr.R
@@ -21,6 +21,7 @@ require(plsdepot)
 source("generate_loading_plot_pdf.r")
 source("../Toolbox/data_reading/load_file_for_regression.r")
 source("run_depot_plsreg.r")
+require(prospectr)
 
 ###### LOAD DATA FROM FILE ###################
 # name of csv file in format of data: ID/wavelengths/Batch/Class
@@ -30,6 +31,9 @@ fileToLoad = "../../Data/Videometer_allbatches_kural_format.csv" #VideometerLab 
 # load chosen file
 DATA <- load_file_for_regression(fileToLoad)
 X <- DATA[[1]]
+#optionally scale the data
+#Xscaled <- savitzkyGolay(X, m=0, p=5, w=27)
+#X <- standardNormalVariate(Xscaled)
 CLASS <- DATA[[3]]
 ###### DATA LOAD END #########################
 
@@ -39,8 +43,8 @@ Xtrain <- X[-seq(1, nrow(X), 4),]
 CLASStrain <- CLASS[-seq(1, length(CLASS), 4)]
 
 #retrieve testing data
-#Xtest <- X[-seq(1, nrow(X), 4),]
-#CLASStest <- CLASS[-seq(1, nrow(X), 4)]
+Xtest <- X[seq(1, nrow(X), 4),]
+CLASStest <- CLASS[seq(1, nrow(X), 4)]
 ###### END ###################################
 
 #### SETTINGS START ##########################
@@ -48,14 +52,15 @@ CLASStrain <- CLASS[-seq(1, length(CLASS), 4)]
 # number of latent components to use for dimension reduction, vector of values allowed
 componentsNumber <- c(1:40)
 # additional file append - scaling or other, will be added to result file name in form Result_addedFileAppend_rest
-addedFileAppend <- "noscale_0.75"
+#addedFileAppend <- "FTIR_SG_SNV_0.75"
+addedFileAppend <- "VM_noscale_0.75"
 # create directory for outputs (does nothing if it exists)
 outputDir = "../Results/PLSREGR_results"
 dir.create(path = outputDir, showWarnings = FALSE)
 #### SETTINGS END ############################
 
 # run algorithm for all components
-run_depot_plsreg(Xtrain, CLASStrain, componentsNumber, outputDir)
+run_depot_plsreg(Xtrain, CLASStrain, Xtest, CLASStest, componentsNumber, outputDir)
 
 # create pdf with loadings plots
 #generate_loading_plot_pdf(LOADINGS = PLSregr$P, filename = paste(outputDir, "/Loadings_Plot_", addedFileAppend, "_.pdf", sep=""), ncompNum = length(componentsNumber))
---
+++ scripts_PLS_regression/run_depot_plsreg.r
@@ -0,0 +1,121 @@
+##################################################
+# Runs PLS regression using algorithms from 
+# plsdepot package.
+# 1. Creates files with observed - predicted comparison
+# Includes information about accuracy
+# 2. Creates files with class association border values
+# Does 1. and 2. for each component number from ncomp
+#
+# Xtrain - matrix with training data for regression
+# CLASStrain - vector of observed values (in intended case
+# translated class association)
+# ncomp - vector of component numbers for which algorithms
+# should be ran
+# outputDir - path to the directory where results will be saved
+#
+# Rafal Kural
+##################################################
+
+
+run_depot_plsreg <- function(Xtrain, CLASStrain, Xtest, CLASStest, ncomp, outputDir) {
+  require(plsdepot)
+
+  #prepare variable for storing plots generated in loop
+  #later will be retrieved and saved in single pdf file
+  savedPlots <- vector(length(ncomp), mode='list')
+  
+  for (componentsNumber_temp in ncomp) {
+    #Display analysis status
+    cat(paste("Processing data for componentNumber: ", componentsNumber_temp, "..\n", sep=""))
+    
+    #perform pls regression
+    PLSregr <- plsreg1(Xtrain, CLASStrain, comps = componentsNumber_temp)
+    #predictedValues <- PLSregr$y.pred #those predictions are for training set
+    #predict values for testing set
+    regressionCoefficients <- as.matrix(PLSregr$reg.coefs[-1])
+    regressionIntercept <- PLSregr$reg.coefs[1]
+    predictedValues <- as.matrix(Xtest) %*% regressionCoefficients + regressionIntercept
+    
+    #round predicted to nearest decimal number
+    #further improvements would include: separate criteria for each range of values
+    #some would give better results when floor'red, some when ceiling'ed, and most would need more complicated means of classification
+    predictedValuesRounded <- round(predictedValues, -1)
+    #create missclasification value vector (by how many % did prediction miss its class)
+    missclassification <- predictedValuesRounded - CLASStest
+    #display how many samples were missclasified overall
+    missCount <- length(which(missclassification != 0))
+    cat(paste("Missclassification occured ", missCount, " times..\n", sep = ""))
+    
+    #create frame for results of test
+    #table will take form: ExpectedClass, PredictedClass
+    resultTable <- data.frame(CLASStest, predictedValues, missclassification, missCount)
+    #save results table in csv file
+    write.csv(resultTable, file = paste(outputDir, "/Result_", addedFileAppend, "_", componentsNumber_temp, "_components", ".csv", sep="")) 
+    
+    #retrieve sample ids by classes
+    id00 <- which(CLASStest == 0)
+    id10 <- which(CLASStest == 10)
+    id20 <- which(CLASStest == 20)
+    id30 <- which(CLASStest == 30)
+    id40 <- which(CLASStest == 40)
+    id50 <- which(CLASStest == 50)
+    id60 <- which(CLASStest == 60)
+    id70 <- which(CLASStest == 70)
+    id80 <- which(CLASStest == 80)
+    id90 <- which(CLASStest == 90)
+    id100 <- which(CLASStest == 100)
+    # push ids to list of vectors
+    id <- list(id00, id10, id20, id30, id40, id50, id60, id70, id80, id90, id100)
+    
+    # retrieve min and max predicted values for each class
+    classGrade <- as.vector(c(0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100))
+    classMin <- rep(0, length(classGrade))
+    classMax <- rep(0, length(classGrade))
+    for (i in 1:length(classGrade)) {
+      classMin[i] <- min(predictedValues[id[[i]]])
+      classMax[i] <- max(predictedValues[id[[i]]])
+    }
+    
+    # generate data table
+    rangeTable <- data.frame(classGrade, classMin, classMax)
+    # save data table as csv file
+    write.csv(rangeTable, file = paste(outputDir, "/Class_Ranges_", addedFileAppend, "_", componentsNumber_temp, "_components", ".csv", sep="")) 
+    
+    #generate plot of observed vs predicted values
+    #prepare regression line + 2 borders
+    regressionLine <- lm(CLASStest ~ predictedValues)
+    CLASStrainBorderTop <- CLASStest + 10
+    CLASStrainBorderBot <- CLASStest - 10
+    upperBorder <- lm(CLASStrainBorderTop ~ predictedValues)
+    bottomBorder <- lm(CLASStrainBorderBot ~ predictedValues)
+    
+    # R tries to open and close window for plots every time
+    #it significantly slows down the script
+    
+    # create regression scatterplot
+    par(mar=c(5.1, 4.1, 4.1, 8.1), xpd=FALSE)
+    plot(CLASStest, predictedValues, col="red", xlab = "Observed", ylab = "Predicted", main = "Regression scatterplot")
+    #add regression line and border lines
+    abline(regressionLine)
+    abline(upperBorder)
+    abline(bottomBorder)
+    
+    #save plot for later use
+    savedPlots[[componentsNumber_temp]] <- recordPlot()
+    
+    #close plot window
+    dev.off()
+    
+    
+  }
+  
+  # create pdf for plot
+  pdf(paste(outputDir, "/Regression_Scatterplot_", addedFileAppend, ".pdf", sep=""))
+  
+  for (plot in savedPlots) {
+    replayPlot(plot)
+  }
+  
+  # close file
+  dev.off()
+}
