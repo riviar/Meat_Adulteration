b70d62d63f920489dd85a2ed0085a253dbc05dd4
Rafal Kural <silvidanfox@gmail.com>
2015-07-20 11:08:33 +0100

    Some fixes.

--- scripts_RF/RF_classification.r
+++ scripts_RF/RF_classification.r
@@ -20,8 +20,8 @@ source("run_rf.r")
 
 ###### LOAD DATA FROM FILE ###################
 # name of csv file in format of data: ID/wavelengths/Batch/Class
-#fileToLoad = "../../Data/Videometer_allbatches_kural_format.csv" #VideometerLab data
-fileToLoad = "../../Data/FTIR_batch4_kural_format.csv" #FTIR data
+fileToLoad = "../../Data/Videometer_allbatches_kural_format.csv" #VideometerLab data
+#fileToLoad = "../../Data/FTIR_batch4_kural_format.csv" #FTIR data
 
 # load chosen file
 DATA <- load_file_for_regression(fileToLoad)
@@ -32,9 +32,9 @@ CLASS <- DATA[[3]]
 
 #VM ONLY
 #remove selected batches
-batchToRemoveIndices <- grep("b2", rownames(X))
-X <- X[-batchToRemoveIndices,]
-CLASS <- CLASS[-batchToRemoveIndices]
+#batchToRemoveIndices <- grep("b2", rownames(X))
+#X <- X[-batchToRemoveIndices,]
+#CLASS <- CLASS[-batchToRemoveIndices]
 
 #FTIR ONLY
 #reduce spectra number
@@ -42,9 +42,9 @@ CLASS <- CLASS[-batchToRemoveIndices]
 #X <- cut_off_by_variance(X, 0.5)
 
 #optionally scale the data
-Xscaled <- savitzkyGolay(X, m=0, p=3, w=27)
+#Xscaled <- savitzkyGolay(X, m=0, p=3, w=27)
 #X <- standardNormalVariate(Xscaled)
-X <- msc(Xscaled)
+#X <- msc(Xscaled)
 ###### DATA LOAD END #########################
 
 ###### SETTING TRAINING AND TEST DATA ########
