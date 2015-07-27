b72788c2ecb7e39bb959332e1d6adf45ca7693bf
Rafal Kural <silvidanfox@gmail.com>
2015-07-27 18:28:28 +0100

    Added scripts for parallel analysis. Each file is processed by separate cpu. Known issue: exp1 FTIR is impossibly long. Splitting it into further parallel analysis is recommended.

---
+++ scripts_performance/Peformance_Test_parallel.r
@@ -0,0 +1,68 @@
+##############################################
+# Tests performance of models created using
+# SVD, PLS-DA, PLS-R, KNN, RF algorithms
+# Generates pdf file with performance/stability graph
+#
+# Add parallelization if possible (should be)
+#
+# Rafal Kural
+##############################################
+
+# clear workspace and open windows
+rm(list = ls())
+graphics.off()
+
+#set working directory to script directory
+test <- dirname(sys.frame(1)$ofile)
+setwd(test)
+
+#loading libraries and required scripts
+source("../Toolbox/data_manipulation/load_file_for_regression.r")
+source("PLSDA_performance.r")
+source("PLSR_performance.r")
+source("SVM_performance.r")
+source("RF_performance.r")
+source("generate_performance_plot.r")
+source("run_Ptest.r")
+
+# parallelisation imports
+library(foreach)
+library(doMC)
+
+######## Performance test settings ############
+# number of CPU cores to use
+registerDoMC(2) 
+# path to data directory
+dataDirectory <- "../../Data/"
+# vector of data file names (each one will get separate analysis and plot)
+dataFileNames <- c("Exp2_FTIR_errors_removed.csv", "Exp2_GCMS.csv", "Exp2_HPLC.csv", "Exp2_VM_errors_removed.csv",
+                   "Exp1_FTIR.csv", "Exp1_microarray.csv", "Exp1_VM.csv")
+# vector of proximity settings for each data file 
+#(exp1 has only beef/mixed/pork classes so we need to include only direct hits)
+proximityAllowed <- c(TRUE, TRUE, TRUE, TRUE, FALSE, FALSE, FALSE)
+
+
+# vector of proximity scores for regression (by how much can result deviate to count as hit)
+# Exp1 has classes 0-beef/1-mixed/2-pork
+# Exp 2 has classes 0/10/20/../100
+allowedDeviation <- c(10, 10, 10, 10, 0.5, 0.5, 0.5)
+
+# ratio of training/test set
+splitRatio <- 0.75
+
+# vector with numbers of latent variables (PLS-DA)
+ncomp <- 4:36
+# max number of components to use (PLS-R)
+maxncomp <- 36
+
+# number of iterations to repeat test
+iterations <- 100
+
+resultsDirectory = "../Results/Performance_Results"
+######## END Performance test settings #########
+
+# run analysis for all files in parallel foreach
+foreach(i=1:length(dataFileNames)) %do% {
+  run_Ptest(dataDirectory = dataDirectory, dataFileNames = dataFileNames[i],
+            proximityAllowed = proximityAllowed[i], allowedDeviation = allowedDeviation[i])
+}
---
+++ scripts_performance/run_Ptest.r
@@ -0,0 +1,56 @@
+########################################
+# Wrapper for parallelization of Performance_Test
+# Runs entire workflow for one file
+#
+# Rafal Kural
+########################################
+
+run_Ptest <- function(dataDirectory, dataFileNames, proximityAllowed, allowedDeviation) {
+  ######## Process data files and load data ######
+  # load data from file
+  DATA <- load_file_for_regression(filepath = paste(dataDirectory, dataFileNames, sep = ""))
+  
+  #extract wavelengths and classes
+  X <- DATA[[1]]
+  CLASS <- DATA[[3]]
+  ######## END Process data files and load data ######
+  
+  
+  ######## PLS-DA performance test ###############
+  PLSDA_scores <- PLSDA_performance(X, CLASS, splitRatio, ncomp, iterations, proximityAllowed)
+  write.csv(file = paste(resultsDirectory, "/PLSDA_Scores_", dataFileNames, sep = ""), x = PLSDA_scores)
+  ######## END PLS-DA performance test ###########
+  
+  ######## PLS regression performance test #######
+  PLSR_scores <- PLSR_performance(X, CLASS, splitRatio, maxncomp, iterations, allowedDeviation)
+  write.csv(file = paste(resultsDirectory, "/PLSR_Scores_", dataFileNames, sep = ""), x = PLSR_scores)
+  ######## END PLS regression performance test ###
+  
+  ######## SVM performance test ##################
+  SVM_Radial_scores <- SVM_performance(X, CLASS, splitRatio, "radial", iterations, allowProximity = proximityAllowed)
+  write.csv(file = paste(resultsDirectory, "/SVM_Radial_Scores_", dataFileNames, sep = ""), x = SVM_Radial_scores)
+  
+  SVM_Polynomial_scores <- SVM_performance(X, CLASS, splitRatio, "polynomial", iterations, allowProximity = proximityAllowed)
+  write.csv(file = paste(resultsDirectory, "/SVM_Polynomial_Scores_", dataFileNames, sep = ""), x = SVM_Polynomial_scores)
+  ######## END SVM performance test ##############
+  
+  ######## RF performance test ###################
+  RF_scores <- RF_performance(X, CLASS, splitRatio, iterations, allowedDeviation)
+  write.csv(file = paste(resultsDirectory, "/RF_scores_", dataFileNames, sep = ""), x = RF_scores)
+  ######## END RF performance test ###############
+  
+  ######## Plot results of test and save in pdf file ##
+  # create results directory if not exists
+  dir.create(path = resultsDirectory, showWarnings = FALSE)
+  
+  # destination file
+  plotFilename <- paste(resultsDirectory, "/Performance_Plot_", dataFileNames, ".pdf", sep = "")
+  # merge data 
+  scoresData <- matrix(PLSDA_scores, nrow=1)
+  scoresData <- rbind(scoresData, PLSR_scores, SVM_Radial_scores, 
+                      SVM_Polynomial_scores, RF_scores)
+  # create vector with names for data
+  scoresNames <- c("PLS-DA", "PLS-R", "SVM Radial", "SVM Polynomial", "RF")
+  # draw plot
+  generate_performance_plot(data = scoresData, names = scoresNames, file = plotFilename)
+}
